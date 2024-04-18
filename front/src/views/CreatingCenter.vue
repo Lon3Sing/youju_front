9bde0446b2d28ee72c0a7b5f05c0835ed8f0fa2f
龙行 <lx21371232@buaa.edu.cn>
2024-04-18 13:35:05 +0800

    创作中心组件导入

--- front/src/views/CreatingCenter.vue
+++ front/src/views/CreatingCenter.vue
@@ -1,11 +1,15 @@
-<script>
-
-</script>
-
 <template>
-
+  <vue-editor v-model="content" />
 </template>
 
-<style scoped lang="scss">
+<script>
+import { VueEditor } from "vue2-editor";
 
-</style>
+export default {
+  components: { VueEditor },
+
+  data: () => ({
+    content: "<h1>Some initial content</h1>"
+  })
+};
+</script>
